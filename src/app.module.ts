6c704d7ce86f9c3281086bc4124cc56b71717aaf
RogerJackD <rogerzapata114@gmail.com>
2025-07-18 18:38:46 -0500

    fature(app)configure Postgresql connection with environment variables

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(), //activacion para uso variables de entorno
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    UserModule //modulos de proyecto...
+  ],
+})
+export class AppModule {}
