fa6d594749ff7be9f6f9b8057064d246400b3f30
RogerJackD <rogerzapata114@gmail.com>
2025-07-19 23:30:03 -0500

    feat(user): add user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()//'increment' by default
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    lastname: string;
+
+    @CreateDateColumn()
+    createAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @DeleteDateColumn()
+    deletedAt : Date;
+}
